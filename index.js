6ae663bd9f32f9008440f1a2090436b9b799ece4
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-05-04 13:26:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var driver = {};
+
+function updateDriverWithKeyAndValue (driver, key, value) {
+  
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+ 
+  return driver;
+}
+
+function deleteFromDriverByKey(){
+  
+}
+
+function destructivelyDeleteFromDriverByKey() {
+  
+}
+
