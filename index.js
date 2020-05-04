389c22c0365ccf7f89d4350744917ff337934c3d
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-05-04 13:37:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var driver = {};
 
 function updateDriverWithKeyAndValue (driver, key, value) {
-  return Driver.assign({},driver,{[key]: value});
+  return Object.assign({}, driver, {[key]: value});
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
@@ -11,7 +11,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function deleteFromDriverByKey(driver, key){
-    var newDriver =  Driver.assign({},driver);
+    var newDriver =  Object.assign({},driver);
   delete newDriver[key];
   return newDriver;
 }
